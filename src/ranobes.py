e0feeb97cde1a9f748d20a705479ec73d336e7e1
Abhinav <60320192+blacksmithop@users.noreply.github.com>
2024-05-12 15:04:59 +0000

    feat: Use comment parser

--- src/ranobes.py
+++ src/ranobes.py
@@ -1,7 +1,10 @@
 from requests_html import HTMLSession
+from src.latest_comments import CommentParser
+
 
 BASE_URL = "https://ranobes.top/index.php"
 
+comment_parser = CommentParser()
 
 class Ranobes:
     def __init__(self, username: str, password: str):
@@ -11,3 +14,9 @@ class Ranobes:
 
     def authenticate_session_with_credentials(self, username: str, password: str):
         self.session.post(f"{BASE_URL}?do=lostpassword", files=dict(login_name=username, login_password=password, login="submit"))
+
+    def get_latest_comments(self):
+        response = self.session.post(f"{BASE_URL}?do=lastcomments")
+        response.html.render()
+        comment_data = comment_parser.parse_data(response.text)
+        return comment_data
